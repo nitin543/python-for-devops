d18420d75e2fdc0a81a0ee9c044a7a75daee0f77
nitin543 <35323662+nitin543@users.noreply.github.com>
2025-07-04 11:07:20 +0000

    functionadded

---
+++ Day-04/functions.py
@@ -0,0 +1,19 @@
+a=10
+b=5
+
+def addition():
+    add = a+b
+    print(add)
+
+def subtraction():
+    sub = a-b
+    print(sub)
+
+def multiplication():
+    mul = a*b
+    print(mul)
+
+
+addition()
+subtraction()
+multiplication()
