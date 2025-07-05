ece9025abf03acce6025f3e09f6b71a564fc54ed
nitin543 <35323662+nitin543@users.noreply.github.com>
2025-07-05 07:55:45 +0000

    lists

---
+++ Day-08/lists.py
@@ -0,0 +1,13 @@
+student_names = ["nitin" ,"puvvada" , "srilekha" , "gamidi"]
+print(type(student_names))
+
+
+print(len(student_names))
+
+student_names.remove("gamidi")
+print(student_names)
+
+print(student_names[0:2])
+
+
+print(student_names[1])
